6b54cf67e4fd8808a7dc24c830d798bf1e7c1443
frej <frejkeyser@gmail.com>
2023-09-28 21:22:14 +0200

    basic setup en file structure

---
+++ src/main/java/nl/frej/dea/spotitube/SpotiTubeApplication.java
@@ -0,0 +1,9 @@
+package nl.frej.dea.spotitube;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class SpotiTubeApplication extends Application {
+}
+
